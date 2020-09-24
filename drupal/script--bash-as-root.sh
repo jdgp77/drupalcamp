23e16923db5c2b17196fe972c6305d5eca89cbae
Julian David Guerrero <jdgp77@gmail.com>
2020-09-23 22:19:26 -0500

    Update Drupal

--- drupal/script--bash-as-root.sh
+++ drupal/script--bash-as-root.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-if [ "$(uname)" == "Darwin" ]
-then
-    docker exec -it drupalcamp_drupal_1 bash
-else
-    docker exec -it --user $(id -u):$(id -g) drupalcamp_drupal_1 bash
-fi
+docker exec -it drupalcamp_drupal_1 bash
--- drupal/web/sites/default/settings.php
+++ drupal/web/sites/default/settings.php
@@ -778,11 +778,11 @@ if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
 $databases['default']['default'] = array (
-  'database' => 'educaci2_drupalcamp',
-  'username' => 'educaci2_drupalc',
-  'password' => 'Vqxik&@3js877JeF_fdR',
+  'database' => 'drupalcamp',
+  'username' => 'root',
+  'password' => 'root',
   'prefix' => '',
-  'host' => 'localhost',
+  'host' => 'db',
   'port' => '3306',
   'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
   'driver' => 'mysql',
